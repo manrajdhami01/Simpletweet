b6f723ce3dae3c113ef39e6d87117e4e0c4f1ab6
manrajdhami01 <manrajdhami007@gmail.com>
2021-03-09 20:56:49 -0500

    Adding tweet functionality

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,10 +5,16 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,18 +26,36 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.TimeZone;
 
+@Parcel
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "userId"))
 public class Tweet {
 
     public static final String TAG = "TweetAct";
 
-    public String body;
-    public String createdAt;
+    @ColumnInfo
+    @PrimaryKey
     public long id;
-    public User user;
-    public String retweetCount;
-    public String favoriteCount;
-    public String embeddedImage;
 
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @ColumnInfo
+    public long userId;
+
+    @Ignore
+    public User user;
+
+    @ColumnInfo
+    public String retweetCount;
+
+    @ColumnInfo
+    public String favoriteCount;
+
+    @ColumnInfo
+    public String embeddedImage;
 
     // Empty constructor needed by the Parceler library
     public Tweet() {}
@@ -42,9 +66,11 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.id = jsonObject.getLong("id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        User user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.user = user;
         tweet.retweetCount = String.valueOf(jsonObject.getString("retweet_count"));
         tweet.favoriteCount = String.valueOf(jsonObject.getString("favorite_count"));
+        tweet.userId = user.id;
         if (jsonObject.getJSONObject("entities").has("media")){
             Log.i(TAG, "the picture link: " + tweet.user);
             tweet.embeddedImage = jsonObject.getJSONObject("entities").getJSONArray("media").getJSONObject(0).getString("media_url_https").substring(0, (jsonObject.getJSONObject("entities").getJSONArray("media").getJSONObject(0).getString("media_url_https")).length()-4) + "?format=jpg&name=large";
